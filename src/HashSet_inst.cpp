ea5bedf5aa370d0de661315083c9fb9e6df5271b
Lorenzo <loridinapoli@gmail.com>
2025-11-16 19:06:08 +0100

    Hash --- Add HashSet ETI

---
+++ src/HashSet_inst.cpp
@@ -0,0 +1,24 @@
+// Hash Library --- HashSet
+#include "../include/LDN/Hash/HashSet.hpp"
+
+// LDN::hash_set::Open 
+template class LDN::hash_set::Open<bool>;
+template class LDN::hash_set::Open<int>;
+template class LDN::hash_set::Open<unsigned int>;
+template class LDN::hash_set::Open<long>;
+template class LDN::hash_set::Open<unsigned long>;
+template class LDN::hash_set::Open<float>;
+template class LDN::hash_set::Open<double>;
+template class LDN::hash_set::Open<char>;
+template class LDN::hash_set::Open<std::string>;
+
+// LDN::hash_set::Close 
+template class LDN::hash_set::Close<bool>;
+template class LDN::hash_set::Close<int>;
+template class LDN::hash_set::Close<unsigned int>;
+template class LDN::hash_set::Close<long>;
+template class LDN::hash_set::Close<unsigned long>;
+template class LDN::hash_set::Close<float>;
+template class LDN::hash_set::Close<double>;
+template class LDN::hash_set::Close<char>;
+template class LDN::hash_set::Close<std::string>;
